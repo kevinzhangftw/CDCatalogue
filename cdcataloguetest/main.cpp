d2a8b92cb61ce9a0602b95e2088342cc928d58f4
Kai Zhang <kwz@asb9700-e06.csil.sfu.ca>
2016-01-18 10:36:31 -0800

    Initial Commit

---
+++ cdcataloguetest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cdcataloguetest
+//
+//  Created by Kai Zhang on 2016-01-18.
+//  Copyright © 2016 Kai Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
